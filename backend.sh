aa81ec1c24b27ab7d76ea7d884e0c75502bcbb35
nagamani <maniece9999@gmail.com>
2025-04-04 16:27:45 +0530

    variables

--- backend.sh
+++ backend.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -52,3 +52,13 @@ then
 else 
      echo -e "expence user already exist.$Y skipping $N"
 fi
+
+mkdir -p /app
+VALIDATING $? "creating /app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting backend application code"
