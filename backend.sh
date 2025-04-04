07c7e7c0e0fbab5d7b8d0b8cd4fb39f41c48675b
nagamani <maniece9999@gmail.com>
2025-04-04 16:30:34 +0530

    variables

--- backend.sh
+++ backend.sh
@@ -46,19 +46,40 @@ VALIDATE $? "Install nodejs"
 id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "expense user not exists. $G creating $N"
+    echo -e "expense user not exists... $G Creating $N"
     useradd expense &>>$LOG_FILE
     VALIDATE $? "Creating expense user"
-else 
-     echo -e "expence user already exist.$Y skipping $N"
+else
+    echo -e "expense user already exists...$Y SKIPPING $N"
 fi
 
 mkdir -p /app
-VALIDATING $? "creating /app folder"
+VALIDATE $? "Creating /app folder"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading backend application code"
 
 cd /app
+rm -rf /app/* # remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# load the data before running backend
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
